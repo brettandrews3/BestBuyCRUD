d109cc8eeea8d91a24ceb78f554105a4e6b87368
Brett Andrews <brettandrews3@gmail.com>
2020-05-07 12:09:29 -0500

    More updates in class

--- BestBuyCRUD/ProductRepository.cs
+++ BestBuyCRUD/ProductRepository.cs
@@ -1,10 +1,30 @@
 ﻿using System;
+using System.Data; // Lets us use IDbConnection
+using Dapper;
+
 namespace BestBuyCRUD
 {
-    public class ProductRepository
+    public class ProductRepository : IProductRepository //ERROR because we need to implement stubbed-out GetAllProducts()
     {
-        public ProductRepository()
+        private readonly IDbConnection _connection; // _private indicator
+        // constructor here will do some setup work for us
+        public ProductRepository(IDbConnection connection)
         {
+            _connection = connection; //taking this _connection and hiding it from outside world (encapsulation)
         }
+
+        public IEnumerable<Product> GetAllProducts() //***instance of Object Relational Mapping (SEE VIDEO FOR BREAKDOWN)
+        {
+           return _connection.Query<Product>("SELECT * FROM Products;");
+        }//Method takes in a Query, sends to database, returns info to us (SEE VIDEO)
+
+    }
+    // GetAllProducts() will need an instance of the ProductRepository class
+
+    //Next, we do CREATE, so we'll use Execute
+    public void CreateProduct(string name, double price, int categoryID)
+    {
+        _connection.Execute("INSERT INTO Products (Name, Price, CategoryID) VALUES (@name, @price, @categoryID);",
+            new { name = name, price = price, categoryID = categoryID });
     }
 }
